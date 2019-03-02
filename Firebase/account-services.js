4fbf70ea62c6f6e203aef3ade3dcd0b16b961b61
morningstar <developermorningstar@gmail.com>
2019-03-02 13:50:06 +0530

    Code style changes

--- Firebase/account-services.js
+++ Firebase/account-services.js
@@ -1,126 +1,125 @@
-var app = require('express')();       //this line gives the variable app a server framework
-var http = require('http').Server(app);         //used to connect to the Server
+var app = require('express')() // this line gives the variable app a server framework
+var http = require('http').Server(app) // used to connect to the Server
 
-var io = require('socket.io')(http);
+var io = require('socket.io')(http)
 
-var admin = require("firebase-admin");
+var admin = require('firebase-admin')
 
-let REGISTRATION_FAILED_MESSAGE = "REGISTRATION_FAILED";
-let REGISTRATION_SUCCESS_MESSAGE = "REGISTRATION_SUCCESS";
-let REGISTRATION_COMPLETE_EVENT = "REGISTRATION_COMPLETE";
-let FIREBASE_AUTH_TOKEN_GENERATED = "FIREBASE_AUTH_TOKEN_GENERATED";
-let FIREBASE_USERS_TABLE = "Users";
-let FIREBASE_PHONE_NUMBERS_TABLE = "Phone Numbers";
+let REGISTRATION_FAILED_MESSAGE = 'REGISTRATION_FAILED'
+let REGISTRATION_SUCCESS_MESSAGE = 'REGISTRATION_SUCCESS'
+let REGISTRATION_COMPLETE_EVENT = 'REGISTRATION_COMPLETE'
+let FIREBASE_AUTH_TOKEN_GENERATED = 'FIREBASE_AUTH_TOKEN_GENERATED'
+let FIREBASE_USERS_TABLE = 'Users'
+let FIREBASE_PHONE_NUMBERS_TABLE = 'Phone Numbers'
 
 var userAccountCreateRequests = (io) => {
-  io.on('connection', (socket)=>{
-    registerUser(socket,io);
-    detectDisconnection(socket,io);
-    logInUser(socket, io);
-  });
-};
+  io.on('connection', (socket) => {
+    registerUser(socket, io)
+    detectDisconnection(socket, io)
+    logInUser(socket, io)
+  })
+}
 
-function registerUser(socket, io){
-  socket.on('userData',(data)=>{
+function registerUser (socket, io) {
+  socket.on('userData', (data) => {
     admin.auth().createUser({
       email: data.email,
       password: data.password,
       displayName: data.username
     })
-    .then((userRecord) => {
-      console.log(`${data.email} was registered successfully`);
+      .then((userRecord) => {
+        console.log(`${data.email} was registered successfully`);
 
-      var db = admin.database();
-      var ref = db.ref(FIREBASE_USERS_TABLE);
-      var userRef = ref.child(data.username);
+        var db = admin.database();
+        var ref = db.ref(FIREBASE_USERS_TABLE);
+        var userRef = ref.child(data.username);
         userRef.set({
           Email: data.email,
           UserName: data.username,
           MobNumber: data.mobNumber,
           IsOnline: false
-        });
+        })
 
         var userStatusRef = db.ref(FIREBASE_PHONE_NUMBERS_TABLE).child(data.mobNumber);
         userStatusRef.set({
-          Email:data.email
+          Email: data.email
         })
 
-      Object.keys(io.sockets.sockets).forEach((id) => {
-        if (id == socket.id){
-          io.to(id).emit(REGISTRATION_COMPLETE_EVENT,REGISTRATION_SUCCESS_MESSAGE);
-        }
-      });
-    })
-    .catch((error) =>{
-      Object.keys(io.sockets.sockets).forEach((id) => {
-        if (id == socket.id){
-          io.to(id).emit(REGISTRATION_COMPLETE_EVENT,REGISTRATION_FAILED_MESSAGE);
-        }
-      });
-    });
-  });
-};
+        Object.keys(io.sockets.sockets).forEach((id) => {
+          if (id === socket.id) {
+            io.to(id).emit(REGISTRATION_COMPLETE_EVENT,REGISTRATION_SUCCESS_MESSAGE);
+          }
+        })
+      })
+      .catch((error) => {
+        Object.keys(io.sockets.sockets).forEach((id) => {
+          if (id === socket.id) {
+            io.to(id).emit(REGISTRATION_COMPLETE_EVENT,REGISTRATION_FAILED_MESSAGE)
+            console.log(error.message)
+          }
+        })
+      })
+  })
+}
 
-
-function logInUser(socket, io){
+function logInUser (socket, io) {
   socket.on('userInfo', (data) =>{
     admin.auth().getUserByEmail(data.email)
-    .then((userRecord) =>{
-      // console.log("Username from userRecord is: "+userRecord.UserName);
-      // console.log(userRecord);
+      .then((userRecord) =>{
+        // console.log("Username from userRecord is: "+userRecord.UserName);
+        // console.log(userRecord);
 
-      var db = admin.database();
-      var ref = db.ref(FIREBASE_USERS_TABLE);
-      var userRef = ref.child(userRecord.displayName);
+        var db = admin.database();
+        var ref = db.ref(FIREBASE_USERS_TABLE);
+        var userRef = ref.child(userRecord.displayName);
 
-      userRef.once('value', (snapshot) =>{
-        var additionalClaims = {
-          email: data.email
-        };
+        userRef.once('value', (snapshot) =>{
+          var additionalClaims = {
+            email: data.email
+          }
 
-        admin.auth().createCustomToken(userRecord.uid, additionalClaims)
-        .then((customToken) =>{
-          Object.keys(io.sockets.sockets).forEach((id) =>{
-            if (id == socket.id) {
-              var token = {
-                authToken: customToken,
-                email: snapshot.val().Email,
-                displayName: snapshot.val().UserName
-              }
-              io.to(id).emit(FIREBASE_AUTH_TOKEN_GENERATED, token);
-            }
-          });
-
-        }).catch((error) =>{
-          Object.keys(io.sockets.sockets).forEach((id) => {
-            if (id == socket.id){
-              var token = {
-                authToken: error.message,
-                email: 'error',
-                displayName: 'error'
-              }
-              io.to(id).emit(FIREBASE_AUTH_TOKEN_GENERATED, token);
-            }
-          });
-        });
-      });
-    })
-    .catch((error) =>{
-      console.log(error.message);
-    });
-  });
+          admin.auth().createCustomToken(userRecord.uid, additionalClaims)
+            .then((customToken) => {
+              Object.keys(io.sockets.sockets).forEach((id) =>{
+                if (id === socket.id) {
+                  var token = {
+                    authToken: customToken,
+                    email: snapshot.val().Email,
+                    displayName: snapshot.val().UserName
+                  }
+                  io.to(id).emit(FIREBASE_AUTH_TOKEN_GENERATED, token);
+                }
+              })
+            }).catch((error) => {
+              Object.keys(io.sockets.sockets).forEach((id) => {
+                if (id === socket.id) {
+                  var token = {
+                    authToken: error.message,
+                    email: 'error',
+                    displayName: 'error'
+                  }
+                  io.to(id).emit(FIREBASE_AUTH_TOKEN_GENERATED, token);
+                }
+              })
+            })
+        })
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  })
 }
 
-function detectDisconnection(socket, io){
-  socket.on('disconnect',()=>{
-  });
+function detectDisconnection (socket, io) {
+  socket.on('disconnect', () => {
+  })
 }
 
 
-function encodeEmail(email){
-  return email.replace('.',',');
+function encodeEmail (email) {
+  return email.replace('.', ',')
 }
 
 module.exports = {
-    userAccountCreateRequests
-  }
+  userAccountCreateRequests
+}
--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-var app = require('express')();       //this line gives the variable app a server framework
-var http = require('http').Server(app);         //used to connect to the Server
+var app = require('express')() // this line gives the variable app a server framework
+var http = require('http').Server(app) // used to connect to the Server
 
-var io = require('socket.io')(http);
+var io = require('socket.io')(http)
 
-var admin = require("firebase-admin");
+var admin = require('firebase-admin')
 
-//Get the firebase credentials from the private folder
-var chattrServiceAccountPath = require(__dirname + "/private/chattrServiceCredentials.json")
+// Get the firebase credentials from the private folder
+var path = require('path')
+var chattrServiceAccountPath = path.join(__dirname, 'private', 'chattrServiceCredentials.json')
+// require(__dirname + '/private/chattrServiceCredentials.json')
 
-//Initialise the firebase credentials of the app.
+// Initialise the firebase credentials of the app.
 admin.initializeApp({
   credential: admin.credential.cert(chattrServiceAccountPath),
-  databaseURL: "https://chattr-d500d.firebaseio.com"
-});
+  databaseURL: 'https://chattr-d500d.firebaseio.com'
+})
 
-//Moved socket code from server.js to account-services.js and called from server.js
-var accountRegistrationReq = require('./Firebase/account-services');
-accountRegistrationReq.userAccountCreateRequests(io);
+// Moved socket code from server.js to account-services.js and called from server.js
+var accountRegistrationReq = require('./Firebase/account-services')
+accountRegistrationReq.userAccountCreateRequests(io)
 
 /*
   This uses an arrow function, new way to define a function.
 */
-http.listen(3000, ()=>{
-  console.log("Server is listening on port 3000");
-});
+http.listen(3000, () => {
+  console.log('Server is listening on port 3000')
+})
